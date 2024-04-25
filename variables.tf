6041b2120cf28ea691665a28fb5dbf541ae1500a
Arpakathy <kathyghomo@gmail.com>
2024-04-25 13:45:13 +0200

    Delete the infrastructure

--- variables.tf
+++ variables.tf
@@ -2,26 +2,6 @@ variable "region" {
   type = string
   default = "us-east-2"
 }
-variable "VPC_cidr" {
-  type = string
-  default = "10.10.0.0/16" 
-}
-variable "subnet_priv1_cidr" {
-  type = string
-  default = "10.10.0.0/20"
-}
-variable "subnet_priv2_cidr" {
-  type = string
-  default = "10.10.16.0/20"
-}
-variable "subnet_pub1_cidr" {
-  type = string
-  default = "10.10.32.0/20"
-} 
-variable "subnet_pub2_cidr" {
-  type = string
-  default = "10.10.80.0/20"
-}  
 variable "AZ1" {
   type = string
   default = "us-east-2a"
